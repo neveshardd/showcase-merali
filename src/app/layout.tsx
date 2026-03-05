f8c89cc0d88d3b94f4284c1810fc110815f49395
neveshardd <118855875+neveshardd@users.noreply.github.com>
2026-03-05 20:55:16 -0300

    fix: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
